608998ff6a155f7c5ec4297632f9c1abf446a6fd
Chia-Chen Yeh <Celine@wonder-iMac.local>
2017-06-27 16:50:06 +0800

    commit2

--- PerfectNumber.py
+++ PerfectNumber.py
@@ -1,18 +1,11 @@
-# encoding: utf-8
+import sys
+
 def is_perfect(num):
-	sum = int(num)
+	sum = 0
 	for i in range(1, int(num) / 2 + 1):
 		if int(num) % i == 0:
-			sum -= i
-	if sum == 0:
-		return "True"
-	else:
-		return "False"
+			sum += i
+	return sum == int(num)
 
-
-while 1:
-	n = raw_input()
-	if n != "-1":
-		print is_perfect(n)
-	else:
-		break
+for i in range(1, len(sys.argv)):
+	print is_perfect(int(sys.argv[i]))
