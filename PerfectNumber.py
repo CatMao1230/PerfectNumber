a8b3e8f29573221a02d88bce6199c0083c60be73
Chia-Chen Yeh <Celine@wonder-iMac.local>
2017-06-28 10:41:52 +0800

    Use list comprehension and range with slice syntax to simplify code

--- PerfectNumber.py
+++ PerfectNumber.py
@@ -2,10 +2,11 @@ import sys
 
 def is_perfect(num):
 	sum = 0
-	for i in range(1, int(num) / 2 + 1):
+	#for i in range(1, int(num) / 2 + 1):
+	for i in [x + 1 for x in range(int(num) / 2)]:
 		if int(num) % i == 0:
 			sum += i
 	return sum == int(num)
 
-for i in range(1, len(sys.argv)):
-	print is_perfect(int(sys.argv[i]))
+for i in sys.argv[1:]:
+	print is_perfect(i)
